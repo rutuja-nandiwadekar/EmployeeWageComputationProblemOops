f3ed3f530174b5a9b11a83ad2677b600831d1258
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-02-13 17:21:27 +0530

    Added: Part time emp and wage logic

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -2,7 +2,7 @@ package com.bridgelabz;
 
 public class EmployeeWage {
 	// initialization
-	public static final int FULL_TIME = 1;
+	public static final int FULL_TIME = 2;
 	public static final int WAGE_PER_HR = 20;
 	public static final int PART_TIME = 1;
 
