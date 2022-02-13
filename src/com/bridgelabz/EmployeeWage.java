ef54ee8ba78623e32cd5208bd3829df56e8a6ade
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-02-13 17:56:43 +0530

    Added: Employeee wages for month logic

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -5,6 +5,7 @@ public class EmployeeWage {
 	public static final int FULL_TIME = 2;
 	public static final int WAGE_PER_HR = 20;
 	public static final int PART_TIME = 1;
+	public static final int WORKING_DAYS = 20;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Problem using Oops concepts");
@@ -12,29 +13,39 @@ public class EmployeeWage {
 	}
 
 	/*
-	 * This method is used to Calculate Daily Employee Wage. using Switch Case
+	 * This method is used to Calculate Wages for a Month. used Switch Case
 	 * Statement
 	 */
 	public static void computeEmpWage() {
 		// variables
 		int FullDayHours = 0;
-		int wage = 0;
-		// Used ((RANDOM)) for Attendance Check
-		int empCheck = (int) (Math.random() * 100) % 3;
-		switch (empCheck) {
-		case FULL_TIME:
-			System.out.println("Employee is Present Full time");
-			FullDayHours = 16;
-			break;
-		case PART_TIME:
-			System.out.println("Employee is Present Part time");
-			FullDayHours = 8;
-			break;
-		default:
-			System.out.println("Employee is Absent");
+		int dailyWage = 0;
+		int totalWage = 0;
+
+		for (int day = 1; day <= WORKING_DAYS; day++) {
+			System.out.println("FOR DAY " + day);
+			// Used ((RANDOM)) for Attendance Check
+			int empCheck = (int) (Math.random() * 100) % 3;
+			// computation
+			switch (empCheck) {
+			case FULL_TIME:
+				FullDayHours = 16;
+				dailyWage = FullDayHours * WAGE_PER_HR;
+				System.out.println("Employee is present for full time \n And Salary is " + dailyWage);
+				break;
+			case PART_TIME:
+				FullDayHours = 8;
+				dailyWage = FullDayHours * WAGE_PER_HR;
+				System.out.println("Employee is present for part time \n And Salary is " + dailyWage);
+				break;
+			default:
+				FullDayHours = 0;
+				dailyWage = FullDayHours * WAGE_PER_HR;
+				System.out.println("Employee is absent \n And Salary is " + dailyWage);
+				break;
+			}
+			totalWage += dailyWage;
 		}
-		// computation
-		wage = FullDayHours * WAGE_PER_HR;
-		System.out.println("Employee Daily Wage is " + wage);
+		System.out.println("Total wage for a month is " + totalWage);
 	}
 }
