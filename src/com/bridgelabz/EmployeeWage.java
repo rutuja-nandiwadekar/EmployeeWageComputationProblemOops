3cecb06c4918bd6b9acf3c72a0082705e5e31395
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-02-13 17:03:34 +0530

    Added: Employee daily wage logic

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -2,7 +2,8 @@ package com.bridgelabz;
 
 public class EmployeeWage {
 	// initialization
-	public static final int IS_PRESENT = 1;
+	public static final int FULL_TIME = 1;
+	public static final int WAGE_PER_HR = 20;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Problem using Oops concepts");
@@ -10,15 +11,23 @@ public class EmployeeWage {
 	}
 
 	/*
-	 * This method is used to check Employee is Present or Absent Used ((RANDOM))
-	 * for Attendance Check
+	 * This method is used to Calculate Daily Employee Wage.
 	 */
 	public static void computeEmpWage() {
+		// variables
+		int FullDayHours = 0;
+		int wage = 0;
 
-		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-		if (empCheck == IS_PRESENT)
+		// Used ((RANDOM)) for Attendance Check
+		int empCheck = (int) (Math.random() * 100) % 2;
+		if (empCheck == FULL_TIME) {
 			System.out.println("Employee is Present");
-		else
+			FullDayHours = 8;
+		} else {
 			System.out.println("Employee is Absent");
+		}
+		// computation
+		wage = FullDayHours * WAGE_PER_HR;
+		System.out.println("Employee Daily Wage is " + wage);
 	}
 }
