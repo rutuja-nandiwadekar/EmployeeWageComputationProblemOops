65492f55a21b4c1c84cf022970b9c95735e131e4
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-02-13 16:00:17 +0530

    Added: Welcome to Employee Wage Computation Problem message

---
+++ src/com/bridgelabz/EmployeeWage.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Problem using Oops concepts");
+	}
+}
