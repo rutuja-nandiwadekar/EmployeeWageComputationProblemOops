9a11ee6abbb773bdf4a9f15e2132baee271cffd9
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-02-13 16:38:04 +0530

    Added: Employee attendance check logic

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,8 +1,24 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
+	// initialization
+	public static final int IS_PRESENT = 1;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Problem using Oops concepts");
+		computeEmpWage(); // calling method
+	}
+
+	/*
+	 * This method is used to check Employee is Present or Absent Used ((RANDOM))
+	 * for Attendance Check
+	 */
+	public static void computeEmpWage() {
+
+		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_PRESENT)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 }
