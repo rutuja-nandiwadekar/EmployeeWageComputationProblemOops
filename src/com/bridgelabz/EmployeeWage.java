57f9df213f6262c7542ff6a3ca238108649f9305
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-02-16 22:24:53 +0530

    Added: Emp wage for multiple companies logic

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,35 +1,32 @@
 package com.bridgelabz;
 
-/*This is refactor Code 
- * to write a Class Method 
- * to Compute Employee Wage 
+/*Program to compute Employee Wage for Multiple Company 
+ * in Procedural Way Using Class Method function parameters
 */
+
 public class EmployeeWage {
-	// initialization
+	// initialization of constant variables
 	public static final int FULL_TIME = 2;
-	public static final int WAGE_PER_HR = 20;
 	public static final int PART_TIME = 1;
-	public static final int WORKING_DAYS = 20;
-	public static final int MAX_WORKING_HRS = 100;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Problem using Oops concepts");
-		computeEmpWage(); // calling method
+		EmployeeWage dMart = new EmployeeWage();
+		dMart.computeEmpWage("Dmart", 25, 22, 100);
+		EmployeeWage relianceDigital = new EmployeeWage();
+		relianceDigital.computeEmpWage("IKEA", 40, 25, 200);
 	}
 
 	/*
-	 * This method is used to Calculate Wages till a condition of total working
-	 * hours or days is reached for a month
+	 * This method is used to Compute Employee Wage for multiple companies
 	 */
-	public static void computeEmpWage() {
+	public int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHours) {
 		// variables
-		int empHrs = 0;
-		int totalEmpHrs = 0;
-		int totalWorkingDays = 0;
+		int empHrs = 0, totalEmpHrs = 8, totalWorkingDays = 0;
 		// computation
-		while (totalEmpHrs <= MAX_WORKING_HRS && totalWorkingDays < WORKING_DAYS) {
+		while (totalEmpHrs < maxHours && totalWorkingDays < numOfWorkingDays) {
 			totalWorkingDays++;
-			// Used ((RANDOM)) for Attendance Check
+			// Using ((RANDOM)) for Attendance Check
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
 			case PART_TIME:
@@ -41,11 +38,14 @@ public class EmployeeWage {
 			default:
 				empHrs = 0;
 			}
-
 			totalEmpHrs += empHrs;
-			System.out.println("For Day" + totalWorkingDays + " : Employee Maximum Working Hours are " + empHrs);
 		}
-		int totalEmpWage = totalEmpHrs * WAGE_PER_HR;
-		System.out.println("Total Employee Wage for month is " + totalEmpWage);
+		int totalEmpWage = totalEmpHrs * empRatePerHour;
+		System.out.println("Company Name : " + company);
+		System.out.println("Employee rate per hour : " + empRatePerHour);
+		System.out.println("Number of working Days : " + numOfWorkingDays);
+		System.out.println("Max working hours per month : " + maxHours);
+		System.out.println("Total Employee Wage for company " + company + " is: " + totalEmpWage);
+		return totalEmpWage;
 	}
 }
